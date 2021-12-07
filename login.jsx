4c81b4af89afd8519880478289473073cdd7f88e
NathanaelCammay <86319861+NathanaelCammay@users.noreply.github.com>
2021-12-07 15:27:25 +0200

    Add files via upload
    The "LogReg" file merges the two login and register forms. the "App" scss file is for the LogReg page.
    
    import "./style.scss";
    
    export { Login } from "./login";
    export { Register } from "./register";

--- login.jsx
+++ login.jsx
@@ -1,9 +1,59 @@
-import React from "react";
-import loginImg from "../../mindworx-logo.png";
+import React, { Component } from "react";
+import loginImg from '../../assets/images/mindworx-logo.png'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import history from './history';
 
 export class Login extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      EmailAddress: "",
+
+      Password: "",
+    };
+
+    this.Password = this.Password.bind(this);
+
+    this.EmailAddress = this.EmailAddress.bind(this);
+
+    this.login = this.login.bind(this);
+  }
+
+  EmailAddress(event) {
+    this.setState({ EmailAddress: event.target.value });
+  }
+
+  Password(event) {
+    this.setState({ Password: event.target.value });
+  }
+
+  login(event) {
+    debugger;
+
+    fetch("https://localhost:44391/Api/loggingin/userlogin", {
+      method: "post",
+
+      headers: {
+        Accept: "application/json",
+
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        EmailAddress: this.state.EmailAddress,
+
+        Password: this.state.Password,
+      }),
+    })
+      .then((Response) => Response.json())
+
+      .then((result) => {
+        console.log(result);
+
+        if (result.Status == "Invalid") alert("Invalid User");
+        else history.push("/admin");
+      });
   }
 
   render() {
@@ -15,12 +65,12 @@ export class Login extends React.Component {
           </div>
           <div className="form">
             <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+              <label htmlFor="EmailAddress">EmailAddress</label>
+              <input type="text" name="EmailAddress" placeholder="EmailAddress" onChange={this.EmailAddress} />
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
+              <input type="password" name="password" placeholder="password" onChange={this.Password} />
             </div>
           </div>
         </div>
@@ -29,7 +79,7 @@ export class Login extends React.Component {
           <a href=""> Forgot Password?</a>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
+          <button onClick={this.login} type="button" className="btn">
             Log In
           </button>
         </div>
--- register.jsx
+++ register.jsx
@@ -1,9 +1,70 @@
-import React from "react";
-import loginImg from "../../mindworx-logo.png";
+import React, { Component } from "react";
+import loginImg from '../../assets/images/mindworx-logo.png'
+import history from './history';
 
 export class Register extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      Username: "",
+
+      Email: "",
+
+      Password: "",
+
+      // Verified:""
+    };
+
+    this.Username = this.Username.bind(this);
+
+    this.Email = this.Email.bind(this);
+
+    this.Password = this.Password.bind(this);
+
+    // this.Verified = this.Verified.bind(this);
+
+    this.register = this.register.bind(this);
+  }
+
+  Username(event) {
+    this.setState({ Username: event.target.value });
+  }
+
+  Email(event) {
+    this.setState({ Email: event.target.value });
+  }
+
+  Password(event) {
+    this.setState({ Password: event.target.value });
+  }
+
+  register(event) {
+    fetch("https://localhost:44391/Api/loggingin/Register", {
+      method: "post",
+
+      headers: {
+        Accept: "application/json",
+
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        Username: this.state.Username,
+
+        Password: this.state.Password,
+
+        Email: this.state.Email,
+
+        // Verified:this.state.Verified,
+      }),
+    })
+      .then((Response) => Response.json())
+
+      .then((Result) => {
+        if (Result.Status == "Success") history.push("/profile");
+        else alert("You are an unauthenticated user.");
+      });
   }
 
   render() {
@@ -16,20 +77,20 @@ export class Register extends React.Component {
           <div className="form">
             <div className="form-group">
               <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+              <input type="text" name="username" placeholder="username" onChange={this.Username} />
             </div>
             <div className="form-group">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" placeholder="email" />
+              <input type="text" name="email" placeholder="email" onChange={this.Email} />
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="text" name="password" placeholder="password" />
+              <input type="text" name="password" placeholder="password" onChange={this.Password} />
             </div>
           </div>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
+          <button onClick={this.register} type="button" className="btn">
             Register
           </button>
         </div>
